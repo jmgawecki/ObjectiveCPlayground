3bbbc7f1b03c914f6018db8d804cb09ab15ffc11
Jakub Gawecki <71930261+jmgawecki@users.noreply.github.com>
2021-03-27 18:26:47 +0000

    Initial Commit

---
+++ ObjectiveCPlayground/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ObjectiveCPlayground
+//
+//  Created by Jakub Gawecki on 27/03/2021.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
